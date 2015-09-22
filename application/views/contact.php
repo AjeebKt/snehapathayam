bef33598e9f7fb1d2a08383967b76af983fe5efa
AjeebKt <ajeebajb@gmail.com>
2015-09-22 14:09:22 +0530

    Change give to donate

--- application/views/contact.php
+++ application/views/contact.php
@@ -35,10 +35,10 @@
 			<div class="menu">
 				<nav>
 					<ul class="navigation-menu">
-						<li><a href="index.php">Home</a></li>
-						<li><a href="give.php">Give</a></li>
-						<li><a href="about.php">About</a></li>
-						<li><a href="contact.php">Contact</a></li>
+						<li><a href="./home">Home</a></li>
+						<li><a href="./donate">Donate</a></li>
+						<li><a href="./about">About</a></li>
+						<li><a href="./contact">Contact</a></li>
 					</ul>
 				</nav>
 				<div class="search">
--- application/views/registerForm.php
+++ application/views/registerForm.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Registerf - Form</title>
+	<title>Register - Form</title>
 	<link rel="stylesheet" type="text/css" href="css/demo.css" />
   <link rel="stylesheet" type="text/css" href="css/normalize.css" />
   <link rel="stylesheet" type="text/css" href="css/style2.css" />
@@ -17,10 +17,10 @@
 			<div class="menu">
 				<nav>
 					<ul class="navigation-menu">
-						<li><a href="index.php">Home</a></li>
-						<li><a href="give.php">Give</a></li>
-						<li><a href="about.php">About</a></li>
-						<li><a href="contact.php">Contact</a></li>
+						<li><a href="./home">Home</a></li>
+						<li><a href="./donate">Donate</a></li>
+						<li><a href="./about">About</a></li>
+						<li><a href="./contact">Contact</a></li>
 					</ul>
 				</nav>
 				<div class="search">
